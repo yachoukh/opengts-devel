caf6d84c6f2f179bff141fd7722c40abbbfca0d3
Mihai Csaky <mihai.csaky@gmail.com>
2015-05-22 08:41:53 +0300

    test_dbdir

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-sed -i 's/#db.sql.rootUser=root/db.sql.rootUser=root/' $GTS_HOME/common.conf
-sed -i "s/#db.sql.rootPass=rootpass/db.sql.rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD/" $GTS_HOME/common.conf
-sed -i "s/db.sql.host=localhost/db.sql.host=$MYSQL_PORT_3306_TCP_ADDR/" $GTS_HOME/common.conf
-update-rc.d mysql enable
+DBDIR=/var/lib/mysql/opengts
+if [ ! -d "$DBDIR" ]; then
+    sed -i 's/#db.sql.rootUser=root/db.sql.rootUser=root/' $GTS_HOME/common.conf
+    sed -i "s/#db.sql.rootPass=rootpass/db.sql.rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD/" $GTS_HOME/common.conf
+    sed -i "s/db.sql.host=localhost/db.sql.host=$MYSQL_PORT_3306_TCP_ADDR/" $GTS_HOME/common.conf
+    update-rc.d mysql enable
+fi
 service mysql start
 cd $GTS_HOME; ant all
-$GTS_HOME/bin/initdb.pl -rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
-$GTS_HOME/bin/dbAdmin.pl -tables=ca
-$GTS_HOME/bin/admin.sh Account -account=sysadmin -nopass -create
+if [ ! -d "$DBDIR" ]; then
+    $GTS_HOME/bin/initdb.pl -rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
+    $GTS_HOME/bin/dbAdmin.pl -tables=ca
+    $GTS_HOME/bin/admin.sh Account -account=sysadmin -nopass -create
+fi
 cp $GTS_HOME/build/*.war $CATALINA_HOME/webapps/
 $CATALINA_HOME/bin/catalina.sh run
 
